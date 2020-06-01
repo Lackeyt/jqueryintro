8591c94f3f912026fca7ca3c1336433e99981b01
Tyson Lackey <lackeyt90@gmail.com>
2020-06-01 15:44:02 -0700

    Update logic in scripts.js for cipher practice
    Co-Authored-by: Lizzie Van Hook <evanhook360@gmail.com>

---
+++ JS/scripts.js
@@ -0,0 +1,26 @@
+const orgString = prompt("Enter a Sentence");
+
+//console.log(orgString);
+
+const newString1 = function(orgString) {
+  return orgString.charAt(0).toUpperCase() + orgString.charAt(orgString.length-1).toUpperCase();
+};
+
+const newString2 = function(newString1) {
+  return newString1(orgString).charAt(newString1(orgString).length-1) + newString1(orgString).charAt(0);
+};
+
+const newString3 = function(orgString, newString2) {
+  return orgString + newString2;
+};
+
+const newString4 = function(orgString, newString3) {
+  return orgString.charAt(Math.floor(orgString.length/2)) + newString3;
+};
+
+
+alert(newString4(orgString, newString3(orgString, newString2(newString1))));
+
+
+
+
